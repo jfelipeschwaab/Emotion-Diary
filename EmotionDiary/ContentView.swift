e719b6a446ada48405f5a55711e497e13dd46457
jfelipeschwaab <joaofsp.games@gmail.com>
2025-09-30 14:27:31 -0300

    Initial Commit

---
+++ EmotionDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmotionDiary
+//
+//  Created by João Felipe Schwaab on 30/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by João Felipe Schwaab on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
